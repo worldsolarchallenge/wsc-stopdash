1450579c745e560d13fe21b5390fc98d17d12361
David Snowdon <3860429+dcasnowdon@users.noreply.github.com>
2023-10-26 14:30:10 +1100

    Add ETA to the dashboard.

--- src/wsc_stopdash/stopdash.py
+++ src/wsc_stopdash/stopdash.py
@@ -6,6 +6,7 @@ import logging
 import flask
 import flask_cachecontrol
 from influxdb_client_3 import InfluxDBClient3
+import math
 import pandas as pd
 
 # Circular import recommended here: https://flask.palletsprojects.com/en/3.0.x/patterns/packages/
@@ -34,6 +35,51 @@ time >= -30d"""
 
     return df
 
+def get_positions(measurement="telemetry", external_only=True):
+    """Get the most recent position information from each car."""
+
+    query = f"""\
+SELECT LAST(latitude),latitude,longitude,*
+FROM "{measurement}"
+WHERE
+class <> 'Other' AND
+{"class <> 'Official Vehicles' AND " if external_only else ""}
+time >= now() - 1d
+GROUP BY shortname"""  # pylint: disable=duplicate-code
+
+    table = client.query(query=query, language="influxql")
+
+    # Convert to dataframe
+    df = (table.to_pandas()
+        .sort_values(by="time")
+    )
+
+    return df
+
+def get_trailering(external_only=True):
+    trailering_query = f"""\
+SELECT MAX(trailering)
+FROM "timingsheet"
+WHERE
+class <> 'Other' AND
+{"class <> 'Official Vehicles' AND " if external_only else ""}
+time >= now() - 7d
+GROUP BY shortname"""  # pylint: disable=duplicate-code
+    trailering_table = client.query(query=trailering_query, language="influxql")
+
+    # Convert to dataframe
+    trailering_df = pd.DataFrame()
+    if len(trailering_table) > 0:
+        trailering_df = (
+            trailering_table.to_pandas()
+            .reset_index()
+            .rename(columns={"max": "trailering"})
+            [["shortname","trailering"]]
+        )
+        return trailering_df
+
+    return pd.DataFrame()
+
 
 @app.route("/")
 def index():
@@ -48,17 +94,57 @@ def stopdash(stopname):
     """Render the control stop template"""
 
     timing_sheet = get_timing_sheet().sort_values(by=["control_stop.number"])
+    positions = get_positions()
+    trailering = get_trailering()
+
+    timing_sheet["trailering"] = False
+
+    if not trailering.empty:
+        df = (timing_sheet
+            .drop(columns=["trailering"])
+            .merge(trailering, on="shortname", how="left", suffixes=("_original",None))
+        )
+
+    if not positions.empty:
+        df = (df
+            .merge(positions, on="shortname", how="left", suffixes=(None,"_positions"))
+        )
+
+    print(df)
 
     print(config["controlstops"])
     stop = config["controlstops"][stopname]
 
     print(timing_sheet[["team", "control_stop.name"]])
 
+    def _calculate_eta(row):
+        speed = row["speed"]
+        if speed < 20.0:
+            return "N/A"
+
+        hours = (stop["km"] - row["distance"]) / speed
+
+        print(f"{row['distance']=}")
+        print(f"{row['speed']=}")
+        print(hours)
+
+        if hours < 0:
+            return "now"
+        elif hours < 1.0:
+            minutes = hours * 60.0
+            return f"{round(minutes)} min"
+        else:
+            # FIXME: If the time is going beyond the EOD indicate "tomorrow", etc.
+            return f"{hours:.1f} hours"
+
+    df["eta"] = df.apply(_calculate_eta, axis=1, result_type="reduce")
+
+    print(df)
     # Grab everything that has passed the previous control point
     entries = (
-        timing_sheet[
-            (timing_sheet["control_stop.number"] >= stop["number"] - 1)
-            & (timing_sheet["control_stop.number"] < stop["number"] + 1)
+        df[
+            (df["control_stop.number"] >= stop["number"] - 1)
+            & (df["control_stop.number"] < stop["number"] + 1)
         ]
         .sort_values(by=["time"])
         .drop_duplicates(subset=["teamnum"], keep="last")
